7b2258348ed581b3466ea1f983d366262330c9c1
Deep Diwani <ddiwani3008@gmail.com>
2017-09-10 13:53:18 +0530

    Update Arduino-code.ino

--- The Lazy Switch/Arduino-code.ino
+++ The Lazy Switch/Arduino-code.ino
@@ -1,5 +1,5 @@
 #include <SoftwareSerial.h>
-SoftwareSerial mySerial(0, 1); // RX, TX
+SoftwareSerial mySerial(0, 1); // RX, TX  //defines that there is communication named as 'mySerial' at 0,1 pins where 0 is receiving pin and 1 is transmitting pin of arduino
 char myChar;
 char prev;
 bool ledon = true;
@@ -8,7 +8,7 @@ void setup()
 {
     Serial.begin(9600);   //communication rate between arduino and pc
     mySerial.begin(38400); //communication rate between arduino and bluetooth module  **this may be different depends on your bluetooth module**
-    pinMode(led1, OUTPUT);
+    pinMode(led1, OUTPUT);  //telling arduino that we'll be using 10th digital pin
 }
 
 void loop()
@@ -18,7 +18,7 @@ void loop()
         myChar = (byte) mySerial.read();  //storing received data into myChar
         Serial.print(myChar);   //just to verify that we are receiving valid data... you can see it on serial monitor(ctrl+Shift+M)
 
-        if(myChar == '1')  
+        if(myChar == '1')
         {
            off1();
         }
@@ -26,7 +26,7 @@ void loop()
         {
            on1();
         }
-        else if(myChar == '5')
+        else if(myChar == '5') //these values(ie 1,2,5) are arbitary u can use characters also but then you have to change in app also..
         {
           pattern();
         }
@@ -36,14 +36,16 @@ void loop()
 
 void on1()
 {
-    digitalWrite(led1 , 1);
+    digitalWrite(led1 , 1);  //10th pin HIGH
     Serial.println(":1st on");
 }
+
 void off1()
 {
-    digitalWrite(led1 , 0);
+    digitalWrite(led1 , 0);  //10th pin LOW
     Serial.println(":1st off");
 }
+
 void pattern()
 {
     Serial.println("started");
@@ -72,4 +74,3 @@ void pattern()
     }
     Serial.println("stopped");
 }
-
